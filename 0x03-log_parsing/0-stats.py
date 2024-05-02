6f7f0082230b6786dcacf20035f6086a538b0a97
LKOUSTIM MOHAMED <132625229+kostum-kirakos@users.noreply.github.com>
2024-05-02 22:45:47 +0100

    log parsing tasks

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+'''
+Module Docs
+'''
+from sys import stdin
+
+
+def print_summary(total_file_size, status_counts):
+    """
+    Print the total file size and counts of HTTP status codes.
+
+    :param total_file_size: Total file size accumulated so far.
+    :param status_counts: Dictionary containing counts of different HTTP status
+    codes.
+    """
+    print("File size: {:d}".format(total_file_size))
+    for code, count in sorted(status_counts.items()):
+        if count != 0:
+            print("{}: {}".format(code, count))
+
+
+# Dictionary to store counts of different HTTP status codes
+http_status_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+
+# Initialize variables
+total_file_size = 0
+line_count = 0
+
+
+try:
+    # Iterate over lines from standard input
+    for line in stdin:
+        # Split the line into words
+        line_args = line.split()
+
+        if len(line_args) > 2:
+            # Extract HTTP status code and file size
+            status_code = line_args[-2]
+            file_size = int(line_args[-1])
+
+            # Update counts in the dictionary
+            if status_code in http_status_counts:
+                http_status_counts[status_code] += 1
+
+            # Update total file size and line count
+            total_file_size += file_size
+            line_count += 1
+
+            # Print summary every 10 lines
+            if line_count == 10:
+                print_summary(total_file_size, http_status_counts)
+                line_count = 0
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Print final summary
+    print_summary(total_file_size, http_status_counts)
