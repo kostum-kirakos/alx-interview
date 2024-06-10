c460bc5e37542bc05418bae2de398f9f2a6422de
LKOUSTIM MOHAMED <132625229+kostum-kirakos@users.noreply.github.com>
2024-06-10 01:38:44 +0100

    Add files via upload

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this module defines the function makeChange
+"""
+
+
+def makeChange(coins, total):
+    """
+    this function takes as arguments
+        coins: list of coins
+        total: the amout to meet using these coins
+    and return the fewest number of coins needed to reach total
+    """
+
+    if total <= 0:
+        return 0
+
+    number_coins = 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            number_coins += 1
+
+    if total == 0:
+        return number_coins
+    return -1
